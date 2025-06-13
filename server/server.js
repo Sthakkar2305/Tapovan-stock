765004da9a2e1bb70b0ff99c8f54df6d4dfa5c82
Sthakkar2305 <132533704+Sthakkar2305@users.noreply.github.com>
2025-06-13 18:00:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: 'https://tapovan-stock.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: false,
+}))
 app.use(express.json())
 
 
