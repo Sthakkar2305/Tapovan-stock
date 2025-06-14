a67e6cb7d2b072501d3f47121630a5b35fc12db6
smit thakkar <sthakkar837@gmail.com>
2025-06-14 23:24:49 +0530

    kuch naya aya hai

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Tapovan School',
+        short_name: 'Tapovan',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0d6efd',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
@@ -10,5 +37,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
+  }
 })
