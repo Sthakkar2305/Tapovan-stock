905ea529d35bf31ef97ad7b1b6501994dcd9bf4e
Smit Thakkar <132533704+Sthakkar2305@users.noreply.github.com>
2026-01-12 19:00:32 +0530

    DB UPDATE

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,15 @@
-import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config()
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-export default sequelize
+export default sequelize;
