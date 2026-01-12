e83873de2758e1c3912a4a555918e90bcc2faa06
Smit Thakkar <132533704+Sthakkar2305@users.noreply.github.com>
2026-01-12 19:01:10 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URLI, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
